e60e8f42620b1dc231b6196f0f7802955ebc853b
Camille Le Bruchec <Camille.lebruchec@gmail.com>
2018-10-02 17:58:33 +0200

    adding errors

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
-let app = require("express")()
+let express = require("express")
+let app = express()
+let bodyParser = require("body-parser")
+let session = require("express-session")
 
+
+// template
 app.set("view engine", "ejs")
 
+
+// middleware
+app.use("/assets", express.static("public"))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(session({
+  secret: 'parapluie',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+
+
+// routes
 app.get('/', (request, response) => {
-  response.render('pages/index', {test: "Coucou"})
+  response.render('pages/index')
 });
+
+app.post('/', (request, response) => {
+  if (request.body.message === "undifined" || request.body.message ==="") {
+    request.session.error = "Erreur 😖"
+    response.render('pages/index', {error: "Pas de message 😢"})
+  }
+})
 
 
 app.listen(8080)
