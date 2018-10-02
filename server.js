cdfb782f242079cdbe1bd7b6bddf8c5a0b25cc65
Camille Le Bruchec <Camille.lebruchec@gmail.com>
2018-10-02 15:34:02 +0200

    adding views

--- server.js
+++ server.js
@@ -3,11 +3,8 @@ let app = require("express")()
 app.set("view engine", "ejs")
 
 app.get('/', (request, response) => {
-  response.send('Coucou')
+  response.render('pages/index', {test: "Coucou"})
 });
 
 
 app.listen(8080)
-
-
-// prout
